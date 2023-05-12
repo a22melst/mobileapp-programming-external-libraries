4ec075b4996e3b58462aa1b0d5ce4dbf727230c1
Melker Stafverfeldt <a22melst@student.his.se>
2023-05-12 15:05:55 +0200

    Implemented the views in MainActivity. Made some minor tweaks to the layout and changed app name.

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
+    Button btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.imageView);
+        btn1 = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
     }
 }
